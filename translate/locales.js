9f1bff05247b2a5d519ea1f2b0df185c577f62d5
eleoromario <eleo.romario@gmail.com>
2024-07-04 16:30:34 -0500

    feat: agregar informacion de la pagina inicio para la traduccion

--- translate/locales.js
+++ translate/locales.js
@@ -1,24 +1,104 @@
 export const locales = {
 	es: {
-		"nav": {
-			"inicio": "inicio",
-			"nosotros": "nosotros",
-			"folac": "folac 2024",
-			"tour": "tours usa",
-			"contacto": "contacto",
-			"español": "español",
-			"ingles": "ingles"
-		}
+		nav: {
+			inicio: "inicio",
+			nosotros: "nosotros",
+			folan: "folan 2024",
+			tour: "tours usa",
+			contacto: "contacto",
+			español: "español",
+			ingles: "ingles",
+		},
+		footer: {
+			description:
+				"Somos una empresa turistica, boutique de viajes en el Perú y en el mundo, brindando servicios de calidad.",
+			contact: "Contactos",
+			service: "Ofrecemos",
+			service01: "Tours y Paquetes en Perú",
+			service02: "Tours en USA",
+			service03: "Tours a Europa",
+			service04: "Tours al Caribe",
+			service05: "Tours especiales",
+		},
+		carousel: {
+			tag: "perú tips",
+			"title-1": "Expertos en viajes",
+		},
+		about: {
+			tag: "Bienvenidos",
+			title: "52º FOLAC Arequipa 2024",
+			text: "El Distrito Múltiple H, Perú viene preparándose con muchísima emoción y entusiasmo, al haber sido designado como anfitrión del 52° FOLAN, siendo sede la hermosa y blanca ciudad de Arequipa que se encuentra a 1,012 kilómetros de distancia de la capital Lima; es la segunda urbe más importante del país y los peruanos nos sentimos orgullosos de recibirlos en esta bella ciudad que tiene como guardián al Volcán Misti y la hermosa Catedral de la Plaza de Armas, los aguardamos deseosos de celebrar el Foro este 2024.",
+			agency: "Agencia autorizada",
+			prices: "Precios especiales",
+			tours: "Tours especializados",
+		},
+		service: {
+			title: "Tours y Programas",
+			consults: "Consultas",
+			see: "ver programa",
+			program01: "FOLAN Arequipa 05 Días",
+			program02: "FOLAN Arequipa 07 Días",
+			program03: "FOLAN Lima y Arequipa 8 Días",
+			program04: "FOLAN Arequipa y Cusco 8 Días",
+			program05: "FOLAN Arequipa y Cusco 9 Días",
+			program06: "FOLAN Lima, Arequipa y Cusco 11 Días",
+		},
+		consult: {
+			title: "Consultas y Reservas",
+			subtitle:
+				"Para reservas ó más información de nuestros servicios...",
+			btn_consult: "Consultas",
+		},
 	},
 	en: {
-		"nav": {
-			"inicio": "home",
-			"nosotros": "about us",
-			"folac": "folac 2024",
-			"tour": "usa tours",
-			"contacto": "contact",
-			"español": "spanish",
-			"ingles": "english"
-		}
-	}
-}
+		nav: {
+			inicio: "home",
+			nosotros: "about us",
+			folan: "folan 2024",
+			tour: "usa tours",
+			contacto: "contact",
+			español: "spanish",
+			ingles: "english",
+		},
+		footer: {
+			description:
+				"We are a tourist company, travel boutique in Peru and in the world, providing quality services.",
+			contact: "Contacts",
+			service: "We offer",
+			service01: "Tours and Packages in Peru",
+			service02: "Tours in USA",
+			service03: "Tours to Europe",
+			service04: "Tours to the Caribbean",
+			service05: "Special tours",
+		},
+		carousel: {
+			tag: "perú tips",
+			"title-1": "Travel experts",
+		},
+		about: {
+			tag: "Welcome",
+			title: "52nd FOLAC Arequipa 2024",
+			text: "The Multiple District H, Perú has been preparing with great emotion and enthusiasm, having been designated as the host of the 52nd FOLAN, with the beautiful and white city of Arequipa being the venue, which is 1,012 kilometers away from the capital Lima; it is the second most important city in the country and Peruvians are proud to welcome you to this beautiful city that has the Misti Volcano as its guardian and the beautiful Cathedral of the Plaza de Armas, we look forward to celebrating the Forum this 2024.",
+			agency: "Authorized agency",
+			prices: "Special prices",
+			tours: "Specialized tours",
+		},
+		service: {
+			title: "Tours and Programs",
+			consults: "Consults",
+			see: "see program",
+			program01: "FOLAN Arequipa 05 Days",
+			program02: "FOLAN Arequipa 07 Days",
+			program03: "FOLAN Lima and Arequipa 8 Days",
+			program04: "FOLAN Arequipa and Cusco 8 Days",
+			program05: "FOLAN Arequipa and Cusco 9 Days",
+			program06: "FOLAN Lima, Arequipa and Cusco 11 Days",
+		},
+		consult: {
+			title: "Consults and Reservations",
+			subtitle:
+				"For reservations or more information about our services...",
+			btn_consult: "Consults",
+		},
+	},
+};
